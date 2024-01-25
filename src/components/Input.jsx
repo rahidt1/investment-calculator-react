aa9f4e5d13c444008999004ff3c768d78955fc2f
Tanvir Ahmed Talukder <rahidt78@gmail.com>
2024-01-25 12:50:41 +0600

    Built static UI.

--- src/components/Input.jsx
+++
@@ -1,8 +0,0 @@
-export function Input({ children }) {
-  return (
-    <p>
-      <label>{children}</label>
-      <input type="number" required />
-    </p>
-  );
-}
--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,15 +1,40 @@
-import { Input } from "./Input";
+import { useState } from "react";
 
 export function InputBox() {
+  const [input, setInput] = useState({
+    initialInvestment: 10000,
+    anualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setInput((prevInput) => {
+      return { ...prevInput, [inputIdentifier]: newValue };
+    });
+  }
+
   return (
     <section id="user-input">
       <div className="input-group">
-        <Input>Initial Investment</Input>
-        <Input>Anual Investment</Input>
+        <p>
+          <label>Initial Investment</label>
+          <input type="number" required />
+        </p>
+        <p>
+          <label>Anual Investment</label>
+          <input type="number" required />
+        </p>
       </div>
       <div className="input-group">
-        <Input>Expected Return</Input>
-        <Input>Duration</Input>
+        <p>
+          <label>Expected Return</label>
+          <input type="number" required />
+        </p>
+        <p>
+          <label>Duration</label>
+          <input type="number" required />
+        </p>
       </div>
     </section>
   );
